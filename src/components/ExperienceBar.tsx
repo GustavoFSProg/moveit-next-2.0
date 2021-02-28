6df1490751397ab33a1efad8a804bff68837b9ea
Gustavo <gustavosohne38@gmail.com>
2021-02-28 09:51:47 -0300

    feiiotEnd

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import styles from '../styles/components/ExpierenceBar.module.css'
 
 export function ExperienceBar() {
   return (
-    <header className="experience-bar">
+    <header className={styles.experienceBar}>
       <span>0</span>
       <div>
         <div style={{ width: '50%' }}>
-          <span style={{ left: '50%' }} className="current-expirence">
+          <span style={{ left: '50%' }} className={styles.currentExpirence}>
             300 xp
           </span>
         </div>
